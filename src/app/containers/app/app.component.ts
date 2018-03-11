ce4fc3401315bb274cdcf250046c60efdf426e1e
Gabriel Nechita <nechita.gabriel@gmail.com>
2018-03-11 17:24:36 +0200

    Extracted app name to it's own class component

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
 
+export class AppComponent {
+  appName  = 'Awesome Bookmarks App';
 }

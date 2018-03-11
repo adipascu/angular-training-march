fc981281706610b2df5abe53e37ea38c2ed43bdf
Gabriel Nechita <nechita.gabriel@gmail.com>
2018-03-11 17:27:30 +0200

    Clarified if condition in template

--- src/app/bookmark-dashboard/components/bookmark-count/bookmark-count.component.ts
+++ src/app/bookmark-dashboard/components/bookmark-count/bookmark-count.component.ts
@@ -1,24 +1,20 @@
 import { Component, Input } from '@angular/core';
-
 import { Bookmark } from '../../models/bookmark.interface';
 
 @Component({
   selector: 'bookmark-count',
   template: `
-    <div>
-      <h3>Our Precious Bookmarks!</h3>
-      <div *ngIf="items?.length > 0">
-        Total bookmarks: {{ items?.length }}
-      </div>
-      <div *ngIf="!items?.length"> Please add some bookmarks</div>
+  <div>
+    <h3>Our Precious Bookmarks!</h3>
+    <div *ngIf="items?.length">
+      Total bookmarks: {{ items?.length }}
     </div>
+    <div *ngIf="!items?.length">Please add some bookmarks</div>
+  </div>
   `
 })
 export class BookmarkCountComponent {
   @Input()
   items: Bookmark[];
-
-  constructor() {
-
-  }
+  constructor() {}
 }
